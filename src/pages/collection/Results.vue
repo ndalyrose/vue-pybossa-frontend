11ba21dd68f7f31ecd84304a45c35e6667e0ad04
Alexander Mendes <alexanderhmendes@gmail.com>
2017-10-02 17:42:55 +0100

    Update results event handlers

--- src/pages/collection/Results.vue
+++ src/pages/collection/Results.vue
@@ -10,7 +10,13 @@
         :is="collectionConfig.results"
         v-else
         :currentUser="currentUser"
-        :collectionConfig="collectionConfig">
+        :results="results"
+        :favourites="favourites"
+        :collectionConfig="collectionConfig"
+        @getresults="onGetResult"
+        @putresult="onPutResult"
+        @postfavourite="onPostFavourite"
+        @delfavourite="onDelFavourite">
       </component>
     </section>
   </div>
@@ -21,7 +27,9 @@ export default {
   data: function () {
     return {
       description: `Browse results of ${this.collectionConfig.name} projects`,
-      navItems: []
+      navItems: [],
+      results: [],
+      favourites: Set()
     }
   },
 
@@ -49,6 +57,62 @@ export default {
   },
 
   methods: {
+    /**
+     * Handle an error.
+     * @param {Object} error
+     *   The error.
+     */
+    handleError (error) {
+      this.$store.dispatch('NOTIFY', {
+        msg: err.exception_msg,
+        type: 'danger'
+      })
+      throw new Error(err)
+    },
+
+    /**
+     * Handle the getresults event.
+     * @param {String} query
+     *   The query.
+     */
+    onGetResults (query) {
+      pybossaApi.post(`/api/results?${query}`).then(r => {
+        this.results = r.data
+      }).catch(err => handleError)
+    },
+
+    /**
+     * Handle the putresult event.
+     * @param {Object} data
+     *   The data.
+     */
+    onPutResult (query) {
+      pybossaApi.put(`/api/results`, data).then(r => {
+        this.results = r.data
+      }).catch(err => handleError)
+    },
+
+    /**
+     * Handle the postfavourite event.
+     * @param {String|Number} taskId
+     *   The task ID.
+     */
+    onPostFavourite (taskId) {
+      pybossaApi.post(`/api/favorites`, { task_id: taskId }).then(() => {
+        favourites.add(taskId)
+      }).catch(err => handleError)
+    },
+
+    /**
+     * Handle the delfavourite event.
+     * @param {String|Number} taskId
+     *   The task ID.
+     */
+    onDelFavourite (taskId) {
+      pybossaApi.delete(`/api/favorites/${taskId}`).then(() => {
+        favourites.delete(taskId)
+      }).catch(err => handleError)
+    }
   },
 
   mounted () {
